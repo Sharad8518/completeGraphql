5af12d046968ace949141d82b9ed45d8523bccee
Pavan Ahirwar <91248254+Sharad8518@users.noreply.github.com>
2022-03-31 13:46:37 +0100

    change
    change

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
-export const MONGO_URI ="mongodb+srv://Ecommerce:pavan123@cluster0.983cd.mongodb.net/UserInformationdb?retryWrites=true&w=majority"
+export const MONGO_URI ="mongodb+srv://Ecommerce:<paasword>@cluster0.983cd.mongodb.net/UserInformationdb?retryWrites=true&w=majority"
 export const  JWT_SECRET ="asdhagsdjsfhdfdjfghd"
